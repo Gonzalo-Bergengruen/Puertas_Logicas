bf780758a33883576aa418e4f6ce60596d0ddd92
Gonzalo-Bergengruen <Gonzalo.Bergengruen@correo.ucu.edu.uy>
2022-09-19 10:48:48 -0300

    Base del codigo hecho.

---
+++ src/Library/Library/AND.cs
@@ -0,0 +1,32 @@
+﻿namespace Compuertas
+{
+    public class AND
+    {
+        public String Nombre {get; set;}
+        public Boolean Valor {get; set;}
+
+        public AND (string nombre, Boolean valor)
+        {
+            this.Nombre = nombre;
+            this.Valor = valor;
+        }
+        public List<AND> AgregarMetodo(string nombre, Boolean valor, List<AND> Compuertas)
+        {
+            AND and1 = new AND(nombre, valor);
+            if (Compuertas.Count == 0)
+            {
+            Compuertas.Add(and1);
+            } else {
+                for (int i = 0; i <= Compuertas.Count; i++)
+                {
+                    if (Compuertas[i].Nombre == and1.Nombre)
+                    { 
+                        Compuertas.Remove(Compuertas[i]);
+                    } 
+                }
+            Compuertas.Add(and1);
+            }
+            return Compuertas;
+        }
+    }
+}
---
+++ src/Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Compuertas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<AND> Compuertas = new List<AND>();
+            AND and1 = new AND ("And-1", false);
+            Compuertas = and1.AgregarMetodo(and1.Nombre, and1.Valor, Compuertas);
+            Console.WriteLine(Compuertas[0].Nombre + " - " + Compuertas[0].Valor);
+            Compuertas = and1.AgregarMetodo(and1.Nombre, true, Compuertas);
+            Console.WriteLine(Compuertas[0].Nombre + " - " + Compuertas[0].Valor);
+        }
+    }
+}
